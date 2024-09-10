c86782017e911d8278427c21cfb209f2015f0634
kirkenj <kirkenj>
2024-09-10 12:27:50 +0300

    Extracted login from jwtBearerEvents into TokenValidationClient

---
+++ Clients/AuthClientService/TokenValidationClient.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System.Security.Cryptography;
+
+namespace Clients.AuthApi.AuthApiIStokenValidClient
+{
+    public interface ITokenValidationClient
+    {
+        public Task<bool> IsTokenValid(string token);
+    }
+
+    public class TokenValidationClient : AuthApiClient, ITokenValidationClient
+    {
+        private static HashAlgorithm? _hashAlgorithm;
+        private static System.Text.Encoding? _hashEncoding;
+
+        public TokenValidationClient(IOptions<AuthClientSettings> baseUrl, IHttpClientFactory httpClient, IHttpContextAccessor contextAccessor) : base(baseUrl, httpClient.CreateClient(nameof(ITokenValidationClient)), contextAccessor)
+        {
+
+        }
+
+        private async Task UpdateEncodingAndHashAlgoritm()
+        {
+            Console.WriteLine($"{nameof(TokenValidationClient)}: Sending request to auth client for hashDefaults.");
+
+            var defaults = await GetHashDefaultsAsync();
+
+            Console.WriteLine($"{nameof(TokenValidationClient)}: Request to auth client for hashDefaults - Success.");
+
+            _hashAlgorithm = HashAlgorithm.Create(defaults.HashAlgorithmName) 
+                ?? throw new ArgumentException($"Couldn't create {nameof(HashAlgorithm)} with given name: '{defaults.HashAlgorithmName}'");
+
+            _hashEncoding = System.Text.Encoding.GetEncoding(defaults.EncodingName);
+        }
+
+        public async Task<bool> IsTokenValid(string token) 
+        {
+            if (_hashAlgorithm == null || _hashEncoding == null)
+            {
+                await UpdateEncodingAndHashAlgoritm();
+            }
+
+            if (_hashAlgorithm == null || _hashEncoding == null)
+            {
+                throw new ApplicationException($"Couldn't get all information about token hashing");
+            }
+
+            _hashAlgorithm.Initialize();
+
+            var tokenHashBytes = _hashAlgorithm.ComputeHash(_hashEncoding.GetBytes(token));
+
+            var tokenHash = _hashEncoding.GetString(tokenHashBytes);
+
+            return await IsTokenValidPOSTAsync(tokenHash);
+        }
+    }
+}
--- Gateway/Gateway/Models/JWT/CustomJwtBearerEvents.cs
+++ Gateway/Gateway/Models/JWT/CustomJwtBearerEvents.cs
@@ -7,65 +7,18 @@ namespace CustomGateway.Models.JWT
 {
     public class CustomJwtBearerEvents : JwtBearerEvents
     {
-        private readonly ITokenValidationClient _authClientService;
-
-        private static HashAlgorithm? _hashAlgorithm;
-        private static System.Text.Encoding? _hashEncoding;
-
+        private readonly ITokenValidationClient _tokenValidationClient;
 
         public CustomJwtBearerEvents(ITokenValidationClient authClientService)
         {
-            _authClientService = authClientService;
+            _tokenValidationClient = authClientService;
         }
-
-        private HashAlgorithm HashAlgorithm
-        {
-            get
-            {
-                if (_hashAlgorithm == null)
-                {
-                    UpdateEncodingAndHashAlgoritm();
-                }
-
-                return _hashAlgorithm ?? throw new ApplicationException($"{nameof(HashEncoding)} is null");
-            }
-        }
-
-        private System.Text.Encoding HashEncoding
-        {
-            get
-            {
-                if (_hashEncoding == null)
-                {
-                    UpdateEncodingAndHashAlgoritm();
-                }
-
-                return _hashEncoding ?? throw new ApplicationException($"{nameof(HashEncoding)} is null");
-            }
-        }
-
-        private void UpdateEncodingAndHashAlgoritm()
-        {
-            Console.Write("Sending request to auth client for hashDefaults. ");
-
-            var task = _authClientService.GetHashDefaultsResponce();
-            task.Wait();
-
-            _hashAlgorithm = HashAlgorithm.Create(task.Result.HashAlgorithmName) ?? throw new ApplicationException();
-            _hashEncoding = System.Text.Encoding.GetEncoding(task.Result.EncodingName);
-            Console.WriteLine("Success.");
-        }
-
 
         public override async Task TokenValidated(TokenValidatedContext context)
         {
-            HashAlgorithm.Initialize();
-
             var token = context.Request.Headers.Authorization.ToString().Split(' ')[1];
 
-            var tokenHashBytes = HashAlgorithm.ComputeHash(HashEncoding.GetBytes(token));
-
-            var result = await _authClientService.IsTokenValid(HashEncoding.GetString(tokenHashBytes));
+            var result = await _tokenValidationClient.IsTokenValid(token);
 
             if (!result)
             {
