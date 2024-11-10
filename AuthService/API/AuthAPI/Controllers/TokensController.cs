bea5e984e1cefc52d0278badb15977d415ccfc82
kirkenj <kirkenj>
2024-11-10 07:15:12 +0300

    Implemented user info component, fixed invalidate token endpoint

--- AuthService/API/AuthAPI/Controllers/TokensController.cs
+++ AuthService/API/AuthAPI/Controllers/TokensController.cs
@@ -1,6 +1,8 @@
 ﻿using AuthAPI.Contracts;
+using Extensions.ClaimsPrincipalExtensions;
 using HashProvider.Contracts;
 using HashProvider.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,10 +28,16 @@ namespace AuthAPI.Controllers
             bool result = await _tokenTracker.IsValid(tokenHash);
             return Ok(result);
         }
-
+        
+        [Authorize]
         [HttpPost("InvalidateUsersToken")]
         public async Task<ActionResult> InvalidateToken(Guid userId)
         {
+            if (!User.IsInRole(Constants.ApiConstants.ADMIN_ROLE_NAME) && User.GetUserId() != userId)
+            {
+                return Forbid();
+            }
+
             await _tokenTracker.InvalidateUser(userId, DateTime.UtcNow);
             return Ok();
         }
